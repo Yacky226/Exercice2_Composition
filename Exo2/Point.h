bce4c710d99a6feff647343d89de373db74c8a38
Yacouba <sawadogoyacouba52@gmail.com>
2024-10-16 19:36:29 +0100

    Ajoutez des fichiers projet.

---
+++ Exo2/Point.h
@@ -0,0 +1,15 @@
+#pragma once
+class Point
+{
+private:
+	double x;
+	double y;
+public:
+	Point(double x = 0.0, double y = 0.0);
+	void Afficher_Point()const;
+	double distance(const Point&) const;
+	void translate(double dx, double dy);
+	bool operator==(const Point&)const;
+	~Point();
+};
+
